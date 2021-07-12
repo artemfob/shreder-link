da9b4db766a395fffc01ee38e4c65c2a27775951
Artem <39241034+artemfob@users.noreply.github.com>
2021-07-12 17:49:50 +0300

    fixed error and organized regisration and login methods
    bad wi-fi router was core of my trouble with 500 error, so I changed network and all works

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -8,6 +8,10 @@ export const useHttp = () =>{
     const request = useCallback(async (url, method='GET',  body = null, headers ={})=>{
         setLoading(true)
         try{
+            if(body){
+                body = JSON.stringify(body) 
+                headers['Content-Type'] = 'application/json'
+            }
             const response = await fetch(url, {method, body, headers})
             const data = await response.json()
             if(!response.ok){
@@ -21,6 +25,6 @@ export const useHttp = () =>{
             throw e
         }
     }, []) 
-    const clearError = () => setError(null)
-    return {loading, request}
+    const clearError = useCallback(()=>setError(null), [])
+    return {loading, request, error, clearError}
  }
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHttp } from "../hooks/http.hook";
+import { useMessage } from './../hooks/message.hook';
 
 export const AuthPage = () => {
-  const { loading, error, request } = useHttp();
+  const message = useMessage()
+  const { loading, error, request, clearError } = useHttp();
   const [form, setForm] = useState({ email: "", password: "" });
+
+  useEffect(()=>{
+    message(error)
+    clearError()
+  }, [error, message, clearError])
 
   const changeHandler = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -12,7 +19,13 @@ export const AuthPage = () => {
   const registerHandler = async () => {
     try {
       const data = await request("/api/auth/register", "POST", { ...form });
-      console.log(data, "data");
+      message(data.message)
+    } catch (e) {}
+  };
+  const loginHandler = async () => {
+    try {
+      const data = await request("/api/auth/login", "POST", { ...form });
+      message(data.message)
     } catch (e) {}
   };
   return (
@@ -47,6 +60,7 @@ export const AuthPage = () => {
           </div>
           <div className="card-action">
             <button
+              onClick={loginHandler}
               className="btn yellow darken-4"
               style={{ marginRight: 10 }}
               disabled={loading}
